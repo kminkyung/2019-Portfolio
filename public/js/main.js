c8c58dc593251e3d45c41090fb839ebc90e94279
kminkyung <mminkyung.k@gmail.com>
2019-11-06 18:02:39 +0900

    문구 수정

--- public/js/main.js
+++ public/js/main.js
@@ -80,35 +80,35 @@ $(".navi-wk").children(".right").click(function(){
 /* SCROLL EVENT */
 $(window).scroll(function(){
 	var scTop = $(this).scrollTop();
-	if(scTop < 900) {
+	if(scTop < 1000) {
 		$(".portfolio-main").css({"background-color":"#ffffff"});
 		$(".portfolio-main").eq(0).find(".caption").stop().animate({"opacity":1, "bottom":"100px"}, 300);
 		$(".portfolio-main").eq(1).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);
 	}
-	else if(scTop < 1800) {
+	else if(scTop < 2000) {
 		$(".portfolio-main").css({"background-color":"#e0e1e1"});
 		$(".portfolio-main").eq(1).find(".caption").stop().animate({"opacity":1, "bottom":"100px"}, 300);
 		$(".portfolio-main").eq(0).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);		
 		$(".portfolio-main").eq(2).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);
 	}
-	else if(scTop < 2700) {
+	else if(scTop < 3000) {
 		$(".portfolio-main").css({"background-color":"#ffb597"});
 		$(".portfolio-main").eq(2).find(".caption").stop().animate({"opacity":1, "bottom":"100px"}, 300);
 		$(".portfolio-main").eq(0).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);		
 		$(".portfolio-main").eq(1).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);		
 	}
-	else if(scTop < 3600) {
+	else if(scTop < 4500) {
 		$(".portfolio-main").css({"background-color":"#cbffcb"});
 		$(".portfolio-main").eq(3).find(".caption").stop().animate({"opacity":1, "bottom":"100px"}, 500);
 		$(".portfolio-main").eq(2).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 500);	
 	}
-	else if(scTop < 4500) {
+	else if(scTop < 6000) {
 		$(".portfolio-main").css({"background-color":"#ffb6c1"});
 		$(".portfolio-main").eq(4).find(".caption").stop().animate({"opacity":1, "bottom":"100px"}, 300);
 		$(".portfolio-main").eq(3).find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);	
 	//  console.log(scTop);
 		}
-	else if(scTop < 5400) {
+	else if(scTop < 7500) {
 		$(".portfolio-main").css({"background-color":"#dbdbf6"});
 		$(".portfolio-main").find(".caption").stop().animate({"opacity":0, "bottom":"-100px"}, 300);	
 	}
